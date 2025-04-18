adf2e3e1a4760792c026e9433091438607494a4e
João Martins <joaodetona@hotmail.com>
2025-04-18 17:30:13 -0300

    pequenas alterações

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -58,7 +58,7 @@
     </div>
     
     <div class="login-footer">
-      <p>&copy; 2023 FinTrack - Todos os direitos reservados</p>
+      <p>&copy; 2025 FinTrack - Todos os direitos reservados</p>
     </div>
   </div>
 </template>
